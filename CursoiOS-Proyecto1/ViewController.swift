30e79a54463b840e67def31ee6913f3f0116396f
Miguel Santiago Rodríguez <msantiago@atsistemas.com>
2022-01-17 11:38:35 +0100

    Initial Commit

---
+++ CursoiOS-Proyecto1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CursoiOS-Proyecto1
+//
+//  Created by Miguel Santiago on 17/1/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
