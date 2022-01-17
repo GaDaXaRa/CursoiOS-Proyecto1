7540bef8012462ee96fbd94f6ec0ae2832cf9dd2
Miguel Santiago Rodríguez <msantiago@atsistemas.com>
2022-01-17 12:09:21 +0100

    fetching data

--- CursoiOS-Proyecto1/ViewController.swift
+++ CursoiOS-Proyecto1/ViewController.swift
@@ -8,12 +8,25 @@
 import UIKit
 
 class ViewController: UIViewController {
+    private let fetchLandmarks: FetchLandmarksUseCase = FetchLandmarksFromDisk()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        fetchData()
     }
 
+    private var landmarks = [Landmark]()
 
 }
 
+
+private extension ViewController {
+    func fetchData() {
+        fetchLandmarks.fetchLandmarks { [weak self] result in
+            switch result {
+            case .success(let landmarks): self?.landmarks = landmarks
+            case .failure(let error): fatalError(error.localizedDescription)
+            }
+        }
+    }
+}
